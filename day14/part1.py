ebf4baeca86205696688f2757757168b057db127
fmortier1 <flo@Florian.local>
2016-12-14 14:02:11 -0800

    day 14

---
+++ day14/part1.py
@@ -0,0 +1,36 @@
+import hashlib
+import sys
+import re
+
+if len(sys.argv) != 2:
+    raise ValueError('Salt is required')
+
+processed = {}
+def get_hash(source):
+    if not source in processed:
+        processed[source] = hashlib.md5(source).hexdigest()
+    return processed[source]
+
+
+index=-1
+found_keys = 0
+salt = 0
+regex = r"(\w)\1{2,}"
+
+while index == -1:
+    test_string = get_hash("{}{}".format(sys.argv[1],salt))
+    matches = re.findall(regex, test_string)
+    if len(matches) > 0:
+        regex2 = "({})\\1{{4,}}".format(matches[0][0])
+        for x in range(salt+1, salt+1000):
+            test_string = get_hash("{}{}".format(sys.argv[1],x))
+            sub_matches = re.findall(regex2, test_string)
+            if len(sub_matches) > 0:
+                found_keys += 1
+                if found_keys == 64:
+                    index=salt
+                    break
+    salt += 1
+print "Index :{}".format(index) 
+        
+        
---
+++ day14/part2.py
@@ -0,0 +1,38 @@
+import hashlib
+import sys
+import re
+
+if len(sys.argv) != 2:
+    raise ValueError('Salt is required')
+
+processed = {}
+def get_hash(source, depth):
+    if not source in processed:
+        previous = source
+        for x in range(0, depth):
+            previous = hashlib.md5(previous).hexdigest()
+        processed[source] = previous
+    return processed[source]
+
+index=-1
+found_keys = 0
+salt = 0
+regex = r"(\w)\1{2,}"
+
+while index == -1:
+    test_string = get_hash("{}{}".format(sys.argv[1],salt), 2017)
+    matches = re.findall(regex, test_string)
+    if len(matches) > 0:
+        regex2 = "({})\\1{{4,}}".format(matches[0][0])
+        for x in range(salt+1, salt+1000):
+            test_string = get_hash("{}{}".format(sys.argv[1],x), 2017)
+            sub_matches = re.findall(regex2, test_string)
+            if len(sub_matches) > 0:
+                found_keys += 1
+                if found_keys == 64:
+                    index=salt
+                    break
+    salt += 1
+print "Index :{}".format(index)
+        
+        
